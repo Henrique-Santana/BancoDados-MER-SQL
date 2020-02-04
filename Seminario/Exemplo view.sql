408a623434f1bfcbd8e4f36e6e3e0c29365e1e12
Henrique Barreiro Santana <47651807863@info.local>
2020-02-04 11:32:12 -0300

    04/02

---
+++ Seminario/Exemplo view.sql
@@ -0,0 +1,104 @@
+CREATE DATABASE TipoMusical_Manhã;
+
+USE TipoMusical_Manhã; 
+
+CREATE TABLE EstilosMusicais (
+ IDEstiloMsuical  INT PRIMARY KEY IDENTITY,
+ Nome VARCHAR (200) NOT NULL 
+ );
+
+ CREATE TABLE Artistas(
+ IDArtista INT PRIMARY KEY IDENTITY,
+ NomeArtista VARCHAR (200) NOT NULL,
+ IDEstiloMsuical int FOREIGN  KEY REFERENCES  EstilosMusicais(IDEstiloMsuical) -- REFERENCES ELE É A PONTE ENTRE UMA TABELA,, OUTRA Generos (IDGenero) NOME TABELA depois ATRIBUTO
+ );
+
+
+
+ --ALTERACAO ADICIONAL COLUNA
+ -- add coluna
+ALTER TABLE Artistas
+ADD DataNascimento DATE;
+SELECT * FROM Artistas;
+
+--
+ ALTER TABLE Atistas
+ MODIFY Nome CHAR;
+
+ --apagar coluna e banco
+ ALTER TABLE Artista
+ DROP COLUMN Nome;
+ SELECT * FROM 
+
+
+ --APAGAR TABELA  
+ DROP TABLE Atistas;
+
+ SELECT * FROM EstilosMusicais;
+
+ ALTER TABLE EstilosMusicais
+ ADD Descricao VARCHAR (200);
+
+ SELECT * FROM EstilosMusicais;
+ SELECT * FROM Artistas;
+
+ --DML LINGUAGEM DE MANIPULACAO DE DADOS
+ 
+ --COMANDO DE INSERIS DADOS
+ INSERT INTO EstilosMusicais (Nome, Descricao)
+ VALUES ('PAGODE','MUSICA BRASILEIRA');
+
+ INSERT INTO EstilosMusicais (Nome, Descricao)
+ VALUES ('PAGODE','MUSICA BRASILEIRA'),
+		('SAMBA','MUSICA RAIZ'),
+		('COUTRY','ESTILO BOA RAPAZ');
+
+INSERT INTO Artistas (NomeArtista, IDEstiloMsuical)
+VALUES ('ZECA PAGODINHO',2),
+		('FRANK',4);
+
+INSERT INTO Artistas (NomeArtista, IDEstiloMsuical)
+VALUES ('SALES',2),
+		('VANILA',4);
+
+
+--UPDATE ALTERAR DADOS DE DENTRO DA TABELA.
+UPDATE EstilosMusicais
+SET Nome = 'FUNK'
+WHERE IDEstiloMsuical = 2;
+
+--DELETE APAGAR ALGUM DADO DA TABELA
+DELETE FROM Artistas
+WHERE IDArtista = 2;
+
+SELECT * FROM vwArtistas;
+SELECT * FROM Artistas;
+
+INSERT INTO  Artistas (NomeArtista, IDEstiloMsuical)
+VALUES ('SINATRA',4),
+		('GENTE BOA',3);
+
+--APAGAR TODOS OS DADOS DA TABELA
+TRUNCATE TABLE ARTISTAS;
+
+
+
+CREATE VIEW -- NÃO PODE ENTRAR NEM SAIR DADOS DELA, É APENAS UMA VISUALIZAÇÃO.....
+vwArtistas AS
+SELECT NomeArtista AS Nome,
+       IDEstiloMsuical AS IDEstilo
+FROM Artistas
+
+SELECT * FROM vwArtistas;
+SELECT * FROM Artistas;
+
+
+
+
+
+CREATE EVENT
+e  /* Nome do Evento*/
+ON SCHEDULE
+EVERY 1 MINUTE /* Configura o Intervalo */
+DO
+INSERT INTO Artistas VALUES (0); /* Declara a ação SQL */
